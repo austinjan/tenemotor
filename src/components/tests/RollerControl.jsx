417405021ce0410e13176a410ce899b31bf8433a
austinjan <austin.jan@gmail.com>
2019-08-26 13:51:38 +0800

    Aletr use global alert context

--- src/components/tests/RollerControl.jsx
+++ src/components/tests/RollerControl.jsx
@@ -1,29 +1,40 @@
 // @flow
-import React, { useState } from "react";
-import { Typography, Row, Col, Alert } from "antd";
+import React, { useState, useContext } from "react";
+import { Typography, Row, Col } from "antd";
+import PollingControlBar from "components/protocol/PollingControlBar";
 import RollerControlPanel from "./RollerCongrolPanel";
 import RollerSelector from "components/roller/RollerSelector";
+import { useTCPSocket } from "libs/tcp/hooks";
+import { AlertContext } from "components/alertutils";
 import has from "ramda/src/has";
 // $FlowFixMe
 import "App.less";
 
 const RollerControl = () => {
   const [currentRoller, setCurrentRoller] = useState({});
-  const [errorState, setError] = useState({ msg: "", diaplay: false });
-  const handleCurrentRollerChanged = (newRoller: { ip: string }) => {
-    console.log("new roller : ", newRoller);
 
+  const alert = useContext(AlertContext);
+  const { api, connecting } = useTCPSocket({
+    onData: data => console.log(data),
+    onError: err => console.log(err)
+  });
+
+  const handleCurrentRollerChanged = newRoller => {
     has("ip", newRoller)
       ? setCurrentRoller(newRoller)
-      : setError({ msg: "Roller missing ip address.", display: true });
+      : alert.error("Invalid IP!");
   };
 
   return (
     <div>
       <Typography.Title level={2}>Roller control panel</Typography.Title>
-      {errorState.display ? <Alert /> : null}
       <RollerSelector currentRollerChanged={handleCurrentRollerChanged} />
-
+      <PollingControlBar
+        roller={currentRoller}
+        onConnect={api.connect}
+        connecting={connecting}
+        onDisconnect={api.disconnect}
+      />
       <Row gutter={16} style={{ marginTop: "16px" }}>
         <Col span={12}>
           <RollerControlPanel title="Upper" />
