191f24fefcca152cc2ced5e5631d99a1c38d6b64
austinjan <austin.jan@gmail.com>
2019-08-27 14:46:17 +0800

    RollerInformationPanel props implement

--- src/components/tests/RollerInformationPanel.jsx
+++ src/components/tests/RollerInformationPanel.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+// @flow
+import React, { useState } from "react";
+// $FlowFixMe
 import "App.less";
 import {
   Typography,
@@ -6,24 +8,49 @@ import {
   Switch,
   InputNumber,
   Card,
-  Divider
+  Divider,
+  Button
 } from "antd";
 
-export default props => {
+type Props = {
+  title: string,
+  phaseCurrent: string | number,
+  motorPeriod: string | number,
+  cardTemperature: string | number,
+  fuzzy: string | number,
+  motorSpeed: string | number,
+  motorOnOff: string | number,
+  motorSpeedExpect: string | number,
+  onOnOffChanged: (v: boolean) => void,
+  onMotorSpeedChanged: (v: number) => void
+};
+const RollerInformationPanel = props => {
   const { title } = props;
+  const {
+    phaseCurrent,
+    motorPeriod,
+    cardTemperature,
+    fuzzy,
+    motorSpeed,
+    motorOnOff,
+    motorSpeedExpect
+  } = props;
+  const { onOnOffChanged, onMotorSpeedChanged } = props;
+  const [speed, setSpeed] = useState(motorSpeedExpect);
+
   return (
     <div>
       <Card title={title}>
         <Typography.Title level={3}>Status</Typography.Title>
-        <Statistic title="Phase Current" value={12} suffix="mA" />
-        <Statistic title="Motor Period" value={9332} suffix="rpm" />
+        <Statistic title="Phase Current" value={phaseCurrent} suffix="mA" />
+        <Statistic title="Motor Period" value={motorPeriod} suffix="rpm" />
         <Statistic
           title="Card Temperature"
-          value={55.67}
+          value={cardTemperature}
           suffix={<span>&deg;C</span>}
         />
-        <Statistic title="Fuzzy MEMF" value={223} suffix="?" />
-        <Statistic title="Motor Speed" value={556} suffix="rpm" />
+        <Statistic title="Fuzzy MEMF" value={fuzzy} suffix="?" />
+        <Statistic title="Motor Speed" value={motorSpeed} suffix="rpm" />
 
         <Divider />
         <div>
@@ -34,15 +61,46 @@ export default props => {
               className="ui__row-item"
               checkedChildren="ON"
               unCheckedChildren="OFF"
-              defaultChecked={false}
+              defaultChecked={motorOnOff}
+              onChange={onOnOffChanged}
             />
           </div>
           <div className="ui__row">
             <span className="ui__row-item">Motor speed: </span>
-            <InputNumber defaultValue={100} className="ui__row-item" />
+            <InputNumber
+              defaultValue={motorSpeedExpect}
+              className="ui__row-item"
+              onChange={v => setSpeed(v)}
+            />
+            <Button
+              type="primary"
+              onClick={() => {
+                onMotorSpeedChanged(speed);
+              }}
+            >
+              Submit
+            </Button>
           </div>
         </div>
       </Card>
     </div>
   );
 };
+
+RollerInformationPanel.defaultProps = {
+  phaseCurrent: 12,
+  motorPeriod: 9332,
+  cardTemperature: 55.42,
+  fuzzy: 223,
+  motorSpeed: 556,
+  motorOnOff: false,
+  motorSpeedExpect: 100,
+  onOnOffChanged: v => {
+    console.log(v);
+  },
+  onMotorSpeedChanged: v => {
+    console.log(v);
+  }
+};
+
+export default React.memo<Props>(RollerInformationPanel);
